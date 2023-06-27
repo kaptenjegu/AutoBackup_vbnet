e9752829b5c326a24f2a464bc88721c61535ea26
kaptenjegu <muhammad.alfan2000@gmail.com>
2023-06-27 13:26:24 +0700

    pertama

---
+++ Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lbl_log = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.cmdRestart = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'lbl_log
+        '
+        Me.lbl_log.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbl_log.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_log.ForeColor = System.Drawing.Color.Lime
+        Me.lbl_log.Location = New System.Drawing.Point(20, 7)
+        Me.lbl_log.Name = "lbl_log"
+        Me.lbl_log.Size = New System.Drawing.Size(444, 30)
+        Me.lbl_log.TabIndex = 0
+        Me.lbl_log.Text = "Label1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'cmdRestart
+        '
+        Me.cmdRestart.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.cmdRestart.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdRestart.Location = New System.Drawing.Point(152, 57)
+        Me.cmdRestart.Name = "cmdRestart"
+        Me.cmdRestart.Size = New System.Drawing.Size(197, 33)
+        Me.cmdRestart.TabIndex = 1
+        Me.cmdRestart.Text = "Restart"
+        Me.cmdRestart.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "Auto Backup File"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(477, 108)
+        Me.Controls.Add(Me.cmdRestart)
+        Me.Controls.Add(Me.lbl_log)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Auto Backup File"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lbl_log As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents cmdRestart As System.Windows.Forms.Button
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Public app_bat, jb_1, jb_2 As String
+    Public loop_int, n_exe As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Me.WindowState = FormWindowState.Minimized
+        lbl_log.Text = "Memulai Aplikasi"
+        Console.WriteLine("start aplikasi")
+        konfig()
+        mulai()
+        Timer1.Start()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        mulai()
+    End Sub
+    Sub mulai()
+        Try
+            Dim waktu As String = TimeOfDay.Hour & ":" & TimeOfDay.Minute
+            If (jb_1 = waktu) Or (jb_2 = waktu) Then
+                Shell(Application.StartupPath & app_bat, AppWinStyle.Hide)
+                'lbl_log.Text = "Mulai Proses backup - freeze 2 menit"
+                lbl_log.Text = TimeOfDay.Hour & ":" & TimeOfDay.Minute & " || auto backup running - " & Date.Now
+                Threading.Thread.Sleep(120000)    'ms - 2 menit
+            End If
+        Catch ex As Exception
+            lbl_log.Text = TimeOfDay.Hour & ":" & TimeOfDay.Minute & " || Error : " & ex.Message
+        End Try
+    End Sub
+    Private Sub konfig()
+        Try
+            Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(Application.StartupPath & "\konfigurasi.txt")
+            Dim hasil As String
+            Dim n As Integer = 1
+            Do
+                hasil = reader.ReadLine
+                If n = 1 Then
+                    app_bat = hasil
+                ElseIf n = 2 Then
+                    jb_1 = hasil
+                ElseIf n = 3 Then
+                    jb_2 = hasil
+                End If
+                'lbl_log.Text &= a
+                n += 1
+            Loop Until hasil Is Nothing
+
+            reader.Close()
+        Catch ex As Exception
+            MsgBox("!!! File tidak lengkap, aplikasi tidak bisa berjalan !!!")
+            End
+        End Try
+    End Sub
+    Private Sub cmdRestart_Click(sender As Object, e As EventArgs) Handles cmdRestart.Click
+        lbl_log.Text = "Memulai Aplikasi"
+        Console.WriteLine("start aplikasi")
+        konfig()
+        mulai()
+        Timer1.Start()
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            Me.Hide()
+            NotifyIcon1.BalloonTipText = "Program dihide di tray"
+            NotifyIcon1.ShowBalloonTip(30)
+        End If
+    End Sub
+
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+    End Sub
+
+End Class
